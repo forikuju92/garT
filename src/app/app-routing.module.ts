d61a5696ecf95fdc34db279db8ece0d4c0054ec6
Funmilayo Olasehinde <forikuju92@outlook.com>
2020-05-24 20:50:47 +0100

    routing configured

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+// import the components for each view to map for routing
+import { AppComponent } from './app.component';
+import { ReqQuoteComponent } from './req-quote/req-quote.component';
+import { GServicesComponent } from './gservices/gservices.component';
+import { GProductsComponent } from './gproducts/gproducts.component';
+import { GContactsComponent } from './gcontacts/gcontacts.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
-
-const routes: Routes = [];
+// Specify browser paths and correspondig view components
+const routes: Routes = [
+  {path: '', redirectTo: '/Home', pathMatch: 'full' },
+  {path: 'Home', component: AppComponent},
+  {path: 'Quote', component: ReqQuoteComponent},
+  {path: 'Services', component: GServicesComponent},
+  {path: 'Products', component: GProductsComponent},
+  {path: 'Contacts', component: GContactsComponent},
+  {path: 'about-us', component: AboutUsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
