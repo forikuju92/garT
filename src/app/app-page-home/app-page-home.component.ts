c10458a11c1b562a103e51a4400df53ec2603c44
Funmilayo Olasehinde <forikuju92@outlook.com>
2020-06-11 11:25:44 +0100

    Worked on navigation

--- src/app/app-page-home/app-page-home.component.ts
+++ src/app/app-page-home/app-page-home.component.ts
@@ -1,13 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-app-page-home',
+  selector: 'app-page-home',
   templateUrl: './app-page-home.component.html',
   styleUrls: ['./app-page-home.component.css']
 })
 export class AppPageHomeComponent implements OnInit {
 
-  constructor() { }
+ constructor() { }
 
   ngOnInit(): void {
   }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HostListener, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'GAR Technical Limited';
+
+  constructor(@Inject(DOCUMENT) private document: Document) { }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    // Get the navbar
+const navbar: HTMLElement = document.getElementById('navbar');
+
+// Get the offset position of the navbar
+const sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+
+if (window.pageYOffset >= sticky) {
+    navbar.classList.add('stickyNav');
+  } else {
+    navbar.classList.remove('stickyNav');
+  }
+  // name = 'Angular';
+  }
 }
+
